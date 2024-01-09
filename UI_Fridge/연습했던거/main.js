e6f5473955b6c09e684d136e48a2f8eed733c829
WHM0304 <whm0304@naver.com>
2024-01-09 22:23:00 +0900

    연습

---
+++ UI_Fridge/연습했던거/main.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const date = new Date();
+  const btn_control = document.querySelector("div.btn_month");
+  const next = document.querySelector(".next");
+  const prev = document.querySelector(".weekdays");
+  let month_num = 0;
+  // document.getElementById("demo").innerHTML = date.toDateString();
+  const day = date.toDateString().substring(3, 7);
+  const change = (document.getElementById("demo").innerHTML = date);
+
+  btn_control.addEventListener("click", (event) => {
+    if (prev) {
+      const date1 = new Date(2024, month_num);
+      document.getElementById("demo").innerHTML = date1;
+      month_num = month_num - 1;
+    }
+  });
+});
